2bb936c7342be69d3cc872dc43aa41318f9a1dda
BrRyMt <brayan.ms.tyco@gmail.com>
2024-08-12 12:10:52 -0500

    v0.41

--- models/Kardex.php
+++ models/Kardex.php
@@ -10,7 +10,7 @@ class Kardex extends Conexion
         $this->pdo = parent::getConexion();
     }
 
-    public function getall($params = [])
+    public function getAll($params = [])
     {
         $query = $this->pdo->prepare("SELECT * FROM vs_kardex WHERE producto = ?");
         $query->execute([$params['producto']]);
@@ -19,30 +19,18 @@ class Kardex extends Conexion
 
     public function add($params = [])
     {
-        $query = $this->pdo->prepare("CALL ");
-        $query->execute([
-            $params['idcolaborador'],
-            $params['idproducto'],
-            $params['tipomovimiento'],
-            $params['cantidad']
-        ]);
-
-        return $query->fetchAll(PDO::FETCH_ASSOC);
-    }
-
-    public function filter($params = [])
-    {
         try {
-            $query = $this->pdo->prepare("CALL ");
-            $query->execute(
-                array(
-                    $params['producto'],
-                    $params['limit_n']
-                )
-            );
-            return $query->fetchAll(PDO::FETCH_ASSOC);
+            $query = $this->pdo->prepare("CALL spu_registrar_kardex(?, ?, ?, ?, ?, ?)");
+            $query->execute([
+                $params['id_producto'],
+                $params['fecha'],
+                $params['tipo_operacion'],
+                $params['motivo'],
+                $params['cantidad'],
+                $params['valor_unitario_historico']
+            ]);
         } catch (Exception $e) {
-            die($e->getMessage());
+            return $e->getMessage();
         }
     }
 }
