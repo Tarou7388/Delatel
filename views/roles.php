b979523e1fb87c1a15d8e2a837b693a165052d22
BrRyMt <brayan.ms.tyco@gmail.com>
2024-08-12 12:09:23 -0500

    v0.41

---
+++ views/roles.php
@@ -0,0 +1,13 @@
+<?php require_once "../header.php"; ?>
+
+
+
+<div class="container-fluid px-4">
+    <h2 class="mt-4">Roles Usuarios</h2>
+    
+</div>
+
+<?php require_once "../footer.php"; ?>
+</body>
+
+</html>
