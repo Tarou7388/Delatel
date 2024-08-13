872e65f9bd72606f2cc64ab81644ecffa7dbf023
Jesús <jesusmattaramos@gmail.com>
2024-08-12 22:00:20 -0500

    index

---
+++ views/FichaTecnicaGpon.php
@@ -0,0 +1,80 @@
+<?php require_once "../header.php"; ?>
+
+<div class="container">
+    <div class="form-container">
+        <h2 class="text-center">Registro</h2>
+        <form>
+            <div class="mb-2">
+                <div class="input-group">
+                    <input type="text" class="form-control" id="codigo-promocional" placeholder="Numero de documento">
+                    <button class="btn btn-outline-primary" type="submit" id="btnBuscarDocumento">Aplicar</button>
+                </div>
+            </div>
+            <div class="mb-2">
+                <input type="text" class="form-control" id="lblNombreCLiente" disabled=true placeholder="Nombre del cliente">
+            </div>
+            <div class="mb-2">
+                <input type="text" class="form-control" id="lblPlanServicio" disabled=true placeholder="Plan de servicio">
+            </div>
+            <hr>
+            <label class="form-label">Parametros del Wireless</label>
+            <div class="row g-3 mb-2">
+                <div class="col md-6">
+                    <input type="text" class="form-control" id="lblBase" placeholder="Base">
+                </div>
+                <div class="col md-6">
+                    <input type="text" class="form-control" id="lblIp" placeholder="IP">
+                </div>
+            </div>
+            <div class="mb-2">
+                <textarea class="form-control" id="confirm-password" placeholder="Señal" rows="3"></textarea>
+            </div>
+            <div class="mb-3">
+                <label for="genero" class="form-label">Género</label>
+                <select class="form-select" id="genero">
+                    <option selected disabled>Selecciona tu género</option>
+                    <option value="masculino">Masculino</option>
+                    <option value="femenino">Femenino</option>
+                    <option value="otro">Otro</option>
+                    <option value="no-declarar">Prefiero no declarar</option>
+                </select>
+            </div>
+            <div class="mb-3">
+                <label for="pais" class="form-label">País</label>
+                <select class="form-select" id="pais">
+                    <option selected disabled>Selecciona tu país</option>
+                    <option value="argentina">Argentina</option>
+                    <option value="mexico">México</option>
+                    <option value="colombia">Colombia</option>
+                    <option value="españa">España</option>
+                    <option value="peru">Perú</option>
+                    <!-- Agrega más países según sea necesario -->
+                </select>
+            </div>
+            <div class="mb-3">
+                <label for="tipo-documento" class="form-label">Tipo de Documento</label>
+                <select class="form-select" id="tipo-documento">
+                    <option selected disabled>Selecciona el tipo de documento</option>
+                    <option value="dni">DNI</option>
+                    <option value="pasaporte">Pasaporte</option>
+                </select>
+            </div>
+            <div class="mb-3">
+                <label for="numero-documento" class="form-label">Número de Documento</label>
+                <input type="text" class="form-control" id="numero-documento" placeholder="Ingresa tu número de documento">
+            </div>
+            <div class="mb-3 form-check">
+                <input type="checkbox" class="form-check-input" id="terminos">
+                <label class="form-check-label" for="terminos">Acepto los <a href="#">términos y condiciones</a></label>
+            </div>
+            <div class="d-grid">
+                <button type="submit" class="btn btn-primary">Registrarse</button>
+            </div>
+        </form>
+    </div>
+</div>
+
+<?php require_once "../footer.php"; ?>
+</body>
+
+</html>
---
+++ views/index.php
@@ -0,0 +1,26 @@
+<?php require_once "../header.php"; ?>
+
+
+
+<div class="container-fluid px-4">
+    <h1 class="mt-4">Dashboard</h1>
+    <ol class="breadcrumb mb-4">
+        <li class="breadcrumb-item active">Dashboard</li>
+    </ol>
+    <div class="card mb-4">
+        <div class="card-header">
+            <i class="fas fa-table me-1"></i>
+            <h1> <strong>
+                    Pagina principal del Dashboard
+            </h1> </strong>
+        </div>
+        <div class="card-body">
+
+        </div>
+    </div>
+</div>
+
+<?php require_once "../footer.php"; ?>
+</body>
+
+</html>
