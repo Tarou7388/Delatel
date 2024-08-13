673b9c82217783d509eea1a92c9f8c91bfa44385
Ingerfaryl <alexis.talla.srv2608@gmail.com>
2024-08-12 21:51:00 -0500

    cambios en fichas eliminación de links

--- views/Ficha_WISP.php
+++ views/Ficha_WISP.php
@@ -1,7 +1,5 @@
 <?php require_once '../header.php'; ?>
 
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-
 <div class="container-fluid px-4">
     <h1 class="mt-4">Control de Averías WISP</h1>
 
