b05186ef39e78a551993b8db47ab51881132f006
Jesús <jesusmattaramos@gmail.com>
2024-08-27 12:21:53 -0500

    roles

--- models/Rol.php
+++ models/Rol.php
@@ -1,40 +1,49 @@
 <?php
 require_once "Conexion.php";
 
-class Rol extends Conexion{
+class Rol extends Conexion
+{
   private $pdo;
 
   public function __construct()
   {
-    $this -> pdo = parent::getConexion();
+    $this->pdo = parent::getConexion();
   }
 
-  public function getAllRol(){
-    $query  =$this->pdo->prepare("SELECT rol from tb_roles");
+  public function getAllRol()
+  {
+    $query  = $this->pdo->prepare("SELECT rol from tb_roles");
     $query->execute();
     return $query->fetchAll(PDO::FETCH_ASSOC);
   }
 
-  public function addRol($params=[]){
-    try{
+  public function addRol($params = [])
+  {
+    try {
       $query = $this->pdo->prepare("CALL spu_registrar_roles(?)");
       $query->execute([
         $params['rol']
       ]);
-    }
-    catch(Exception $e){
+    } catch (Exception $e) {
       return $e->getMessage();
     }
   }
-  public function getPermisos($data = []){
-    try{
+  public function getPermisos($data = [])
+  {
+    try {
       $consulta = $this->pdo->prepare("CALL spu_listar_permisos(?)");
       $consulta->execute([
         $data['rol']
       ]);
-      return $consulta->fetchAll(PDO::FETCH_ASSOC);
-    }
-    catch(Exception $e){
+
+      $resultado = $consulta->fetchAll(PDO::FETCH_ASSOC);
+      foreach ($resultado as &$row) {
+        if (isset($row['permisos'])) {
+          $row['permisos'] = json_decode($row['permisos'], true);
+        }
+      }
+      return $resultado;
+    } catch (Exception $e) {
       return $e->getMessage();
     }
   }
