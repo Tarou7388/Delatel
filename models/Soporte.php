157d3709df9000e0294260d8a2dccb0760c228ad
Ingerfaryl <alexis.talla.srv2608@gmail.com>
2024-08-30 22:43:25 -0500

    avance de soporte

--- models/Soporte.php
+++ models/Soporte.php
@@ -14,9 +14,21 @@ class Soporte extends Conexion
   {
     try {
       $status = false;
-      $query = $this->pdo->prepare("CALL spu_registrar_fichasoporte(?)");
-      $Json = json_encode($params['wisp']);
-      $status = $query->execute([$Json]);
+      $query = $this->pdo->prepare("CALL spu_registrar_fichasoporte(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+      $pagosJson = json_encode($params['pagos']);
+      $soporteJson = json_encode($params['soporte']);
+      $status = $query->execute([
+        $params['id_contrato'],
+        $params['id_tipo_soporte'],
+        $params['id_tecnico'],
+        $params['fecha_hora_solicitud'],
+        $params['fecha_hora_asistencia'],
+        $params['descripcion_problema'],
+        $params['descripcion_solucion'],
+        $params['prioridad'],
+        $pagosJson,
+        $soporteJson
+      ]);
       return $status;
     } catch (Exception $e) {
       return $e->getMessage();
