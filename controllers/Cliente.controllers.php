86d440c18719916fadc6977c0cb76bc5389e5495
BrRyMt <Brayan.ms.tyco@gmail.com>
2024-09-11 15:24:05 -0500

    cliente cambios

--- controllers/Cliente.controllers.php
+++ controllers/Cliente.controllers.php
@@ -11,3 +11,40 @@ if(isset($_GET["operacion"])){
       break;
   }
 }
+
+if (isset($_POST['operacion'])) {
+  switch ($_POST['operacion']) {
+    case 'add':
+      $datos = [
+        "idPersona"             => $_POST['idPersona'],
+        "idempresa"             => $_POST['idempresa'],
+        "direccion"             => $_POST['direccion'],
+        "referencia"            => $_POST['referencia'],
+        "iduser_create"         => $_POST['iduser_create']
+      ];
+      $estado = $producto->add($datos);
+      echo json_encode(["Guardado" => $estado]);
+      break;
+  }
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'PUT') {
+  $inputData = file_get_contents('php://input');
+  $data = json_decode($inputData, true);
+
+  if (isset($data['id_cliente'])) {
+    $datos = [
+      "idPersona"             => $data['idPersona'],
+      "idempresa"             => $data['idempresa'],
+      "direccion"             => $data['direccion'],
+      "referencia"            => $data['referencia'],
+      "iduser_create"         => $data['iduser_create'],
+      "id_cliente"            => $data['id_cliente'],
+    ];
+
+    $estado = $producto->updateProducto($datos);
+    echo json_encode(["Actualizado" => $estado]);
+  } else {
+    echo json_encode(["Actualizado" => false, "error" => "ID de producto no encontrado"]);
+  }
+}
--- models/Cliente.php
+++ models/Cliente.php
@@ -2,45 +2,54 @@
 
 require_once 'Conexion.php';
 
-class Cliente extends Conexion{
-    
+class Cliente extends Conexion
+{
+
     private $pdo;
 
-    public function add($data = []){
-        try{
-            $consulta = $this->pdo->prepare("CALL spu_clientes_registrar(?,?,?,?)");
+    public function __CONSTRUCT()
+    {
+        $this->pdo = parent::getConexion();
+    }
+
+
+    public function add($data = [])
+    {
+        try {
+            $consulta = $this->pdo->prepare("CALL spu_clientes_registrar(?,?,?,?,?)");
             $consulta->execute(array(
                 $data["idPersona"],
                 $data["idempresa"],
                 $data["direccion"],
-                $data["referencia"]
+                $data["referencia"],
+                $data["iduser_create"]
             ));
-        }
-        catch(Exception $e){
+        } catch (Exception $e) {
             die($e->getMessage());
         }
     }
 
-
-    public function update($data = []) : bool {
+    public function update($data = []): bool
+    {
         $status = false;
-        try{
-            $consulta = $this->pdo->prepare("CALL spu_clientes_registrar(?,?,?,?)");
+        try {
+            $consulta = $this->pdo->prepare("CALL spu_clientes_actualizar(?,?,?,?,?,?)");
             $status = $consulta->execute(array(
                 $data["id_persona"],
                 $data["id_empresa"],
                 $data["direccion"],
                 $data["referencia"],
-                $data["iduser_update"]
+                $data["iduser_update"],
+                $data["id_cliente"],
             ));
             return $status;
-        }
-        catch(Exception $e){
+        } catch (Exception $e) {
             die($e->getMessage());
         }
     }
 
-    public function getByDoc($params=[]){
+    public function getByDoc($params = [])
+    {
         $consulta = $this->pdo->prepare("CALL sp_buscar_cliente_doc (?)");
         $consulta->execute(
             [$params['doc']]
