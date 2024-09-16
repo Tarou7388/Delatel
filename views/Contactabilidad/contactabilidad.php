baf0a8270540f242dced5e05732a20906ba26036
Ingerfaryl <alexis.talla.srv2608@gmail.com>
2024-09-16 12:39:20 -0500

    avance contactabilidad

--- views/Contactabilidad/contactabilidad.php
+++ views/Contactabilidad/contactabilidad.php
@@ -5,17 +5,51 @@
       <h2>Contactabilidad</h2>
     </div>
     <div class="card-body">
-      <div class="form-floating mb-3 mt-3">
-        <input type="text" class="form-control" id="txtNombreContactabilidad" placeholder="Nombre">
-        <label for="lblNombreContactabilidad">Nombre</label>
-      </div>
-      <div class="form-floating mb-3 mt-3">
-        <input type="text" class="form-control" id="txtApellidosContactabilidad" placeholder="Apellidos">
-        <label for="lblApellidosContactabilidad">Apellidos</label>
-      </div>
+      <form action="" class="form-control" id="frmContactabilidad">
+        <div class="row mt-1 mb-2">
+          <div class="col-md-4 mb-3 mt-3">
+            <div class="input-group">
+              <div class="form-floating flex-fill">
+                <input type="number" class="form-control" id="txtDniContactabilidad" placeholder="DNI">
+                <label for="lblDniContactabilidad">DNI</label>
+              </div>
+              <button class="btn btn-primary" type="button" id="btnBuscar"><i class="fa-solid fa-magnifying-glass"></i></button>
+            </div>
+          </div>
+          <div class="col-md-4 mb-3 mt-3">
+            <div class="form-floating">
+              <input type="text" class="form-control" id="txtNombreContactabilidad" placeholder="Nombre">
+              <label for="lblNombreContactabilidad">Nombre</label>
+            </div>
+          </div>
+          <div class="col-md-4 mb-3 mt-3">
+            <div class="form-floating">
+              <input type="text" class="form-control" id="txtApellidosContactabilidad" placeholder="Apellidos">
+              <label for="lblApellidosContactabilidad">Apellidos</label>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-5">
+            <div class="form-floating">
+              <input type="number" class="form-control" id="txtNumeroContactabilidad" placeholder="Número de Celular">
+              <label for="lblCelularContactabilidad">Numero de Celular</label>
+            </div>
+          </div>
+          <div class="col-md-7">
+            <div class="form-floating">
+              <input type="email" class="form-control" id="txtEmailContactabilidad" placeholder="Email">
+              <label for="">Correo electronico</label>
+            </div>
+          </div>
+        </div>
+      </form>
+
+
+
       datos completos
-dni 
-dirección y ubicación en tiempo actual 
+      dni
+      dirección y ubicación en tiempo actual
     </div>
   </div>
 </div>
