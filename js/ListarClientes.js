f989c3a33f1b051aba3139e24cd67093a5308a71
Tarou <jesusmattaramos@gmail.com>
2024-09-18 15:53:21 -0500

    gpon

--- js/ListarClientes.js
+++ js/ListarClientes.js
@@ -72,42 +72,12 @@ window.tablaClientes = $('#listarCliente').DataTable({
             title: 'Acciones',
             render: function (data, type, row) {
                 return `
-<button class="btn btn-warning btn-edit" data-id="${row.codigo_cliente}"><i class="fa-regular fa-pen-to-square"></i></button>
-<button class="btn btn-danger btn-delete" data-id="${row.codigo_cliente}"><i class="fa-regular fa-trash-can"></i></button>
+<button class="btn btn-warning btn-edit" data-id="${row.id_cliente}"><i class="fa-regular fa-pen-to-square"></i></button>
+<button class="btn btn-danger btn-delete" data-id="${row.id_cliente}"><i class="fa-regular fa-trash-can"></i></button>
 `;
             }
         }
     ]
 });
-$('#listarCliente tbody').on('click', '.btn-edit', function () {
-    const idcliente = $(this).data('id');
 
-    fetch(`${config.HOST}controllers/cliente.controllers.php?operacion=getByDoc&numDoc=${idcliente}`)
-        .then(response => response.json())
-        .then(data => {
-            console.log(data);
-            if (idcliente.toString().length >= 8 && idcliente.toString().length <= 10) {
-                $('#editNombrePersona').val(data.nombre);
-                // let nombreParts = data.nombre.split(', ');
-                // if (nombreParts.length === 2) {
-                //     $('#editNombrePersona').val(nombreParts[1]);
-                //     $('#editApellidosPersona').val(nombreParts[0]);
-                // }
-                $('#editEmailPersona').val(data.email);
-                $('#editTelefonoPersona').val(data.telefono);
-                $('#editDireccionPersona').val(data.direccion);
-                $('#editReferenciaPersona').val(data.referencia);
-                $('#editCoordenadasPersona').val(data.coordenadas);
-                $('#editPersonaModal').modal('show');
-            } else if (idcliente.toString().length == 11) {
-                $('#editNombreEmpresa').val(data.nombre);
-                $('#editEmailEmpresa').val(data.email);
-                $('#editTelefonoEmpresa').val(data.telefono);
-                $('#editDireccionEmpresa').val(data.direccion);
-                $('#editReferenciaEmpresa').val(data.referencia);
-                $('#editCoordenadasEmpresa').val(data.coordenadas);
-                $('#editEmpresaModal').modal('show');
-            }
-        })
-        .catch(error => console.error('Error fetching product:', error));
-});
+
