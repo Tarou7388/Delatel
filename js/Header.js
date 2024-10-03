08c22daa2dffd8d9d344518e40b29f605e239cad
Ingerfaryl <alexis.talla.srv2608@gmail.com>
2024-10-03 12:01:25 -0500

    saad

--- js/Header.js
+++ js/Header.js
@@ -6,19 +6,19 @@ window.addEventListener("DOMContentLoaded", () => {
     const Personas = $("#molPersonas");
     const Roles = $("#molRoles");
 
-    // if(permisos[0].permisos.soporte?.leer){
-    //     Soporte.removeAttribute('hidden');
-    // }
-    // if(permisos[0].permisos.contratos?.leer){
-    //     Contratos.removeAttribute('hidden');
-    // }
-    // if(permisos[0].permisos.inventariado?.leer){
-    //     Inventariado.removeAttribute('hidden');
-    // }
-    // if(permisos[0].permisos.personas?.leer){
-    //     Personas.removeAttribute('hidden');
-    // }
-    // if(permisos[0].permisos.roles?.leer){
-    //     Roles.removeAttribute('hidden');
-    // }
+    if(permisos[0].permisos.soporte?.leer){
+        Soporte.removeAttribute('hidden');
+    }
+    if(permisos[0].permisos.contratos?.leer){
+        Contratos.removeAttribute('hidden');
+    }
+    if(permisos[0].permisos.inventariado?.leer){
+        Inventariado.removeAttribute('hidden');
+    }
+    if(permisos[0].permisos.personas?.leer){
+        Personas.removeAttribute('hidden');
+    }
+    if(permisos[0].permisos.roles?.leer){
+        Roles.removeAttribute('hidden');
+    }
 });
