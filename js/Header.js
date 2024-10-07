1a3a1191f1874b425e2b5747da4c945021433667
EloyAlexander69 <delacruzpenalozae3@gmail.com>
2024-10-06 21:43:29 -0500

    Permisos

--- js/Header.js
+++ js/Header.js
@@ -1,49 +1,42 @@
 window.addEventListener("DOMContentLoaded", () => {
-  function $(id) {
-    return document.querySelector(id);
-  }
-  const Soporte = $("#molSoporte");
-  const Contratos = $("#molContratos");
-  const Inventariado = $("#molInventariado");
-  const Clientes = $("#molClientes");
-  const Usuarios = $("#molUsuarios");
-  const Roles = $("#molRoles");
+    function $(id) { return document.querySelector(id) }
+    const Soporte = $("#molSoporte");
+    const Contratos = $("#molContratos");
+    const Inventariado = $("#molInventariado");
+    const Usuario = $("#molUsuarios");
+    const Clientes = $("#molClientes");
+    const Roles = $("#molRoles");
 
-    console.log(permisos[0].permisos);
-  if (permisos[0].permisos.soporte?.leer) {
-    Soporte.removeAttribute("hidden");
-  } else {
-    Soporte.setAttribute("hidden", true); // Asegúrate de ocultar
-  }
-
-  if (permisos[0].permisos.contratos?.leer) {
-    Contratos.removeAttribute("hidden");
-  } else {
-    Contratos.setAttribute("hidden", true); // Asegúrate de ocultar
-  }
-
-  if (permisos[0].permisos.inventariado?.leer) {
-    Inventariado.removeAttribute("hidden");
-  } else {
-    Inventariado.setAttribute("hidden", true); // Asegúrate de ocultar
-  }
-
-  if (permisos[0].permisos.personas?.leer) {
-    Clientes.removeAttribute("hidden");
-  } else {
-    Clientes.setAttribute("hidden", true); // Asegúrate de ocultar
-  }
-
-  if (permisos[0].permisos.personas?.leer) {
-    Usuarios.removeAttribute("hidden");
-  } else {
-    Usuarios.setAttribute("hidden", true); // Asegúrate de ocultar
-  }
-
-  if (permisos[0].permisos.roles?.leer) {
-    Roles.removeAttribute("hidden");
-  } else {
-    Roles.setAttribute("hidden", true); // Asegúrate de ocultar
-  }
-
+    if (permisos.length > 0 && permisos[0].permisos) {
+        if(permisos[0].permisos.soporte?.leer){
+            Soporte.removeAttribute('hidden');
+        }else {
+            Soporte.setAttribute('hidden', true); 
+        }
+        if(permisos[0].permisos.contratos?.leer){
+            Contratos.removeAttribute('hidden');
+        }else {
+            Contratos.setAttribute('hidden', true); 
+        }
+        if(permisos[0].permisos.inventariado?.leer){
+            Inventariado.removeAttribute('hidden');
+        }else {
+            Inventariado.setAttribute('hidden', true);
+        }
+        if(permisos[0].permisos.personas?.leer){
+            Clientes.removeAttribute('hidden');
+        }else {
+            Clientes.setAttribute('hidden', true); 
+        }
+        if(permisos[0].permisos.personas?.leer){
+            Usuario.removeAttribute('hidden');
+        }else {
+            Usuario.setAttribute('hidden', true); 
+        }
+        if(permisos[0].permisos.roles?.leer){
+            Roles.removeAttribute('hidden');
+        }else {
+            Roles.setAttribute('hidden', true); 
+        }
+    }
 });
