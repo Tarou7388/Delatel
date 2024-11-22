72dce40441bdf2822e56880080cd224f94502892
Tarou <jesusmattaramos@gmail.com>
2024-11-22 10:02:47 -0500

    lineas

--- app/DB/Procedimientos/Procedimientos_Cajas.sql
+++ app/DB/Procedimientos/Procedimientos_Cajas.sql
@@ -14,3 +14,18 @@ CREATE PROCEDURE spu_mufas_listar()
 BEGIN
   SELECT id_mufa, nombre, descripcion, coordenadas, id_sector FROM tb_mufas;
 END$$
+
+CREATE TABLE tb_lineas (
+    id_linea INT PRIMARY KEY AUTO_INCREMENT,
+    id_sector INTEGER NOT NULL,
+    coordenadas JSON NOT NULL,
+    iduser_create INTEGER NOT NULL,
+    date_create DATE NOT NULL DEFAULT NOW()
+);
+INSERT INTO tb_lineas(id_sector, coordenadas, iduser_create) VALUES (1, '[
+    {"lat": -13.411086708881479, "lng": -76.16067944369733},
+    {"lat": -13.415915837032298, "lng": -76.14137781811166},
+    {"lat": -13.416000192545276, "lng": -76.14082079571914},
+    {"lat": -13.416418725232612, "lng": -76.139855179086},
+    {"lat": -13.415263815402367, "lng": -76.13952708343515}
+]', 1);
