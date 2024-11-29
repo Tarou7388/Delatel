b357151a411692ecfe04cb68cf35bde033ff6415
BrRyMt <Brayan.ms.tyco@gmail.com>
2024-11-29 11:00:54 -0500

    Soporte Fibra

--- js/SoporteFibr.js
+++ js/SoporteFibr.js
@@ -33,6 +33,18 @@ document.addEventListener("DOMContentLoaded", () => {
     return urlParams.get("idsoporte");
   };
 
+  /**
+   * Documentacion sobre que hace:
+   * Llenado de datos del cliente y soporte de fibra.
+   *
+   * @async
+   * @function llenadoDeDatos
+   * @param {string} doct - Documento del cliente.
+   * @param {number} idSoporte - ID del soporte.
+   * @param {string} tiposervicio - Tipo de servicio.
+   * @returns {Promise<void>} - Promesa que se resuelve cuando los datos han sido llenados.
+   * @throws {Error} - Lanza un error si ocurre un problema al obtener los datos del cliente o del soporte.
+   */
   async function llenadoDeDatos(doct, idSoporte, tiposervicio) {
     try {
       const respuesta = await fetch(`${config.HOST}/app/controllers/Cliente.controllers.php?operacion=buscarClienteDoc&valor=${doct}`);
@@ -51,9 +63,26 @@ document.addEventListener("DOMContentLoaded", () => {
 
     try {
       const dataFibra = await FichaSoporte(idSoporte);
-      const fibraFiltrado = JSON.parse(dataCable[0].ficha_instalacion).cable;
-    } catch (error) {
+      const fibraFiltrado = JSON.parse(dataFibra[0].ficha_instalacion).fibraOptica;
+      console.log(fibraFiltrado);
+      //Asignacion para el campo de plan.
+      console.log(fibraFiltrado.plan);
+      
+      //
+      console.log(fibraFiltrado);
 
+      //
+      console.log(fibraFiltrado);
+
+      //
+      console.log(fibraFiltrado);
+
+      //
+      console.log(fibraFiltrado);
+
+
+    } catch (error) {
+      console.error("Error en data de Fibra:", error);
     }
   }
 });
