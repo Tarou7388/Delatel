1ea33f96eed92bfc686f0b2f5839a9cabcff6446
Alex <alexis.talla.srv2608@gmail.com>
2024-12-13 11:15:16 -0500

    reportes con sd

--- app/controllers/Clientes.ssp.php
+++ app/controllers/Clientes.ssp.php
@@ -8,9 +8,6 @@ $dotenv->load();
 
 $host = $_ENV['HOST'];
 
-echo $host;
-
-
 $columns = array(
   array('db' => 'id_cliente', 'dt' => 0), // ID Cliente
   array('db' => 'nombre_cliente', 'dt' => 1), // Nombre Cliente
@@ -20,7 +17,7 @@ $columns = array(
     'dt' => 3,
     'formatter' => function ($d, $row) use ($host) { // Agregar $host como variable externa
       $nombreCliente = urlencode($row['nombre_cliente']);
-      $contratoUrl = "$host/Delatel/views/Reportes/tablaContrato?id=$d&nombre=$nombreCliente";
+      $contratoUrl = "$host/views/Reportes/tablaContrato?id=$d&nombre=$nombreCliente";
       return '
         <button class="btn btn-primary btn-contrato" onclick="window.location.href=\'' . $contratoUrl . '\'">Ver Contrato</button>
       ';
@@ -30,7 +27,7 @@ $columns = array(
     'db' => 'id_cliente',
     'dt' => 4,
     'formatter' => function ($d, $row) use ($host) { // Agregar $host como variable externa
-      $detallesUrl = "$host/Delatel/views/reports/Cliente/soporte.php?id=$d";
+      $detallesUrl = "$host/views/reports/Cliente/soporte.php?id=$d";
       return '
         <button class="btn btn-secondary btn-detalles" onclick="window.open(\'' . $detallesUrl . '\', \'_blank\')">Ver Detalles</button>
       ';
